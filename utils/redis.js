b5aa368b276742dff5f5e881dc1d91170913bef9
dennismutitu <dennismutitu06@gmail.com>
2024-08-07 10:21:24 +0300

    bug fix for redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,11 +1,13 @@
+// utils/redis.js
 const redis = require('redis');
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
 
+    // Display any error of the redis client in the console
     this.client.on('error', (err) => {
-      console.error('Redis client creation failure:', err);
+      console.error('Redis client error:', err);
     });
   }
 
@@ -14,40 +16,42 @@ class RedisClient {
   }
 
   async get(key) {
-    return new Promise((res, rej) => {
+    return new Promise((resolve, reject) => {
       this.client.get(key, (err, reply) => {
         if (err) {
-          rej(err);
+          reject(err);
         } else {
-          res(reply);
+          resolve(reply);
         }
       });
     });
   }
 
   async set(key, value, duration) {
-    return new Promise((res, rej) => {
-      this.client.setex(key, value, duration, (err, reply) => {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
         if (err) {
-          rej(err);
+          reject(err);
         } else {
-          res(reply);
+          resolve(reply);
         }
       });
     });
   }
 
   async del(key) {
-    return new Promise((res, rej) => {
+    return new Promise((resolve, reject) => {
       this.client.del(key, (err, reply) => {
         if (err) {
-          rej(err);
+          reject(err);
         } else {
-          res(reply);
+          resolve(reply);
         }
       });
     });
   }
 }
+
+// Create and export an instance of RedisClient
 const redisClient = new RedisClient();
 module.exports = redisClient;
