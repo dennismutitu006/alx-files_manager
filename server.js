a0002e1824fdbb9de8f7a7339785aabbb26fc8c3
dennismutitu <dennismutitu06@gmail.com>
2024-08-07 13:44:33 +0300

    express server creation 2.1

---
+++ server.js
@@ -0,0 +1,12 @@
+import routes from './routes/index.js';
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
